34444274ac21b59626b6fa2d7b84786a8c475b8a
Scaxlibur <scaxlibur@gmail.com>
2025-05-22 19:36:40 +0800

    refactor(ADS1256): 优化初始化配置并添加详细注释
    重构了 ADS1256 的初始化配置，添加了时钟和传感器检测电流源的控制。同时，为枚举类型和寄存器配置添加了详细的注释，以提高代码的可读性和可维护性。

--- ADS1256/ads1256.c
+++ ADS1256/ads1256.c
@@ -4,10 +4,6 @@ uint8_t wreg = 0x50;
 uint8_t regnumber = 0x00;
 uint8_t IO = 0x00;
 
-uint8_t data1=0;
-uint8_t data2=0;
-uint8_t data3=0;
-
 void ADS1256WREG(uint8_t regaddr,uint8_t databyte)
 {
 	wreg += (regaddr & 0x0F);    
@@ -31,9 +27,11 @@ void ADS1256WREG(uint8_t regaddr,uint8_t databyte)
 void ADS1256_Init(void)
 {
 	//初始化过程参考手册
-	ADS_1256_CMD_t cmd = ADS1256_CMD_SELFCAL;
-	ADS1256_DRATE_t drate = ADS1256_DRATE_10SPS; // 10k采样率
-	ADS1256_GAIN_t gain = ADS1256_GAIN_1;// 0x00,PGA为1
+	const ADS_1256_CMD_t cmd = ADS1256_CMD_SELFCAL;				// 启动失调和增益自校准
+	const ADS1256_DRATE_t drate = ADS1256_DRATE_10SPS; 			// 10k采样率
+	const ADS1256_GAIN_t gain = ADS1256_GAIN_1;					// 正负5V最大值
+	const ADS1256_CLOCK_t clock = CLOCK_ON;						// 时钟不分频输出
+	const ADS1256_SENSOR_CURRENT_SOURCE_t sensor = SENSOR_OFF;	// 关闭传感器检测电流源
   	while(ADS1256_DRDY);
 	CS_0();
 	HAL_SPI_Transmit(&hspi1,(uint8_t *)&cmd,1,100);
@@ -41,10 +39,10 @@ void ADS1256_Init(void)
 	CS_1();
 	//**********************************
 
-	ADS1256WREG(ADS1256_STATUS,0x06);               // 启用自校准和模拟输入缓冲区
-	// ADS1256WREG(ADS1256_STATUS,0x04);               //
-	// ADS1256WREG(ADS1256_MUX,0x08);                  //
-	ADS1256WREG(ADS1256_ADCON,gain);        
+	ADS1256WREG(ADS1256_STATUS,0b00000110);	// 启用自校准和模拟输入缓冲区
+	// ADS1256WREG(ADS1256_STATUS,0x04);
+	// ADS1256WREG(ADS1256_MUX,0x08);
+	ADS1256WREG(ADS1256_ADCON,gain | clock | sensor);
 	ADS1256WREG(ADS1256_DRATE,drate);  
 	ADS1256WREG(ADS1256_IO,IO);               
 	while(ADS1256_DRDY);
@@ -57,6 +55,9 @@ void ADS1256_Init(void)
 
 uint32_t ADS1256ReadData(uint8_t channel)  
 {
+	uint8_t data1=0;
+	uint8_t data2=0;
+	uint8_t data3=0;
 	uint32_t sum=0;
 	ADS_1256_CMD_t cmd1 = ADS1256_CMD_SYNC;
 	ADS_1256_CMD_t cmd2 = ADS1256_CMD_WAKEUP;
@@ -70,7 +71,7 @@ uint32_t ADS1256ReadData(uint8_t channel)
 	HAL_SPI_Transmit(&hspi1,(uint8_t *)&cmd2,1,100);	               
 	HAL_SPI_Transmit(&hspi1,(uint8_t *)&cmd3,1,100);
 	//接收数据依次存入data中
-  HAL_SPI_TransmitReceive(&hspi1,(uint8_t *)0xff,(uint8_t *)&data1,1,100);
+  	HAL_SPI_TransmitReceive(&hspi1,(uint8_t *)0xff,(uint8_t *)&data1,1,100);
 	HAL_SPI_TransmitReceive(&hspi1,(uint8_t *)0xff,(uint8_t *)&data2,1,100);
 	HAL_SPI_TransmitReceive(&hspi1,(uint8_t *)0xff,(uint8_t *)&data3,1,100);
 	sum |= (data1 <<16);
